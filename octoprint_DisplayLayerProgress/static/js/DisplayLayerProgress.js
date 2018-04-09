3a63afdddb0bde2f545a986d13782947f3766d44
Olli <olaf.kaus@gmail.com>
2018-04-09 16:25:10 +0200

    naked plugin-skeleton

---
+++ octoprint_DisplayLayerProgress/static/js/DisplayLayerProgress.js
@@ -0,0 +1,29 @@
+/*
+ * View model for DisplayLayerProgress
+ *
+ * Author: Olli
+ * License: AGPLv3
+ */
+$(function() {
+    function DisplaylayerprogressViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: DisplaylayerprogressViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_DisplayLayerProgress, #tab_plugin_DisplayLayerProgress, ...
+        elements: [ /* ... */ ]
+    });
+});
